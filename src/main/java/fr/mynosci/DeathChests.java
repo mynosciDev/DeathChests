340e46a09d80412f53ab3a593ad44a6d6097be8f
mynosci <mynosci@gmail.com>
2026-08-15 23:04:07 +0200

    initial commit

--- src/main/java/fr/mynosci/DeathChests.java
+++ src/main/java/fr/mynosci/DeathChests.java
@@ -2,7 +2,7 @@ package fr.mynosci;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class deathchests extends JavaPlugin {
+public final class DeathChests extends JavaPlugin {
 
     @Override
     public void onEnable() {
